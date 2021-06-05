4ccebb857dac63a48ea168fcedbfeab3f481384b
Trol00311 <80637774+Trol00311@users.noreply.github.com>
2021-06-05 15:11:44 +0200

    configure: slider, contact, etc

--- test.js
+++ test.js
@@ -5,3 +5,21 @@ document.getElementById("contactTitle").value="";
 document.getElementById("contactMessage").value="";
 
 }
+
+function slider1(){
+ document.getElementById("test1").style.visibility="visible";
+ document.getElementById("test2").style.visibility="hidden";
+ document.getElementById("test3").style.visibility="hidden";
+}
+
+function slider2(){
+    document.getElementById("test1").style.visibility="hidden";
+    document.getElementById("test2").style.visibility="visible";
+    document.getElementById("test3").style.visibility="hidden";
+}
+ 
+function slider3(){
+    document.getElementById("test1").style.visibility="hidden";
+    document.getElementById("test2").style.visibility="hidden";
+    document.getElementById("test3").style.visibility="visible";
+}
